b5109287e3b3b3f3889269a4679eb34b776cbe6e
parikhk <parikhkushal9@gmail.com>
2019-09-16 15:01:51 +0530

    13 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -18,6 +18,9 @@ import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
 
+import { AuthService } from './auth/auth.service';
+import { TrainingService } from './training/training.service';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { HelloComponent } from './hello.component';
@@ -26,6 +29,7 @@ import { HelloComponent } from './hello.component';
   imports:      [ 
     BrowserModule, 
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule,
     AppRoutingModule,
@@ -45,6 +49,7 @@ import { HelloComponent } from './hello.component';
     SidenavListComponent,
     StopTrainingComponent
     ],
+  providers: [AuthService,TrainingService],
   bootstrap:    [ AppComponent ],
   entryComponents: [StopTrainingComponent]
 })
--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -8,14 +10,33 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class SidenavListComponent implements OnInit {
 
   @Output() closeSidenav = new EventEmitter<void>();
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
   }
 
   onClose() {
     this.closeSidenav.emit();
+  }
+
+  onLogout() {
+    this.onClose();
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
 }
